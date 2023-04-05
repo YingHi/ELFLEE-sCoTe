173c51ee1e78fd57765c84a12201721354152ad0
Ogdoabyss <101399129+YingHi@users.noreply.github.com>
2023-04-05 23:14:59 +0900

    3주차

---
+++ 3Week/source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+const int ROW = 14;
+const int COL = 56;
+const char MAP[ROW][COL] = { // 입력된 맵
+    // 여기에 맵을 입력해주세요
+};
+
+class Node {
+public:
+    int x, y; // 노드의 좌표
+    vector<Node> adj_list; // 인접 리스트
+    bool visited;
+
+    Node(int x, int y) {
+        this->x = x;
+        this->y = y;
+        visited = false;
+    }
+};
+
+Node createNode(int x, int y) {
+    if (MAP[x][y] != '0') { // P 또는 E가 있는 칸인 경우
+        return new Node(x, y);
+    }
+    return NULL;
+}
+
+void buildGraph(vector<Node>& graph) {
+    // 각 칸에 대해 노드 생성 및 인접 리스트 구성
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            Node node = createNode(i, j);
+            if (node != NULL) {
+                if (i > 0) {
+                    Node* top = createNode(i - 1, j);
+                    if (top != NULL) {
+                        node->adj_list.push_back(top);
+                        top->adj_list.push_back(node);
+                    }
+                }
+                if (j > 0) {
+                    Node* left = createNode(i, j - 1);
+                    if (left != NULL) {
+                        node->adj_list.push_back(left);
+                        left->adj_list.push_back(node);
+                    }
+                }
+                graph.push_back(node);
+            }
+        }
+    }
+}
